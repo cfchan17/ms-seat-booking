00b3e702a23255aae7c38ce8ddf23a23ab56f590
cfchan17 <cfchan17@gmail.com>
2022-12-19 11:14:16 +0800

    Initial commit

---
+++ ms-seat-booking-svc/src/main/java/com/cfchan/ms/seat/booking/svc/SeatBookingApplication.java
@@ -0,0 +1,11 @@
+package com.cfchan.ms.seat.booking.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeatBookingApplication.class, args);
+    }
+}
